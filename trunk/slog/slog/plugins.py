4b6bf8475131068d93aedbe69e4984076f173519
renatn <renatn@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2008-02-07 20:48:29 +0000

    Реализован поиск и загрузка модуля

--- trunk/slog/slog/plugins.py
+++ trunk/slog/slog/plugins.py
@@ -9,25 +9,26 @@ class PluginManager:
 		self.plugins = []
 
 	def scan_for_plugins(self):
-		plugins_dir = "../plugins"
-		path = os.path.abspath(plugins_dir)
-		print path
-		os.chdir(path)
-		print "Current:", os.getcwd()
+
+		plugins_dir = os.path.abspath("../plugins")
+
 		for modname in os.listdir(plugins_dir):
+
+			print "Modname:", modname
+
 			path = os.path.join(plugins_dir, modname)
-			self.plugins.append(path)
+
+			print "Path:", path
+
 			if "__init__.py" in os.listdir(path):
 
-				
 				(stream, path, desc) = imp.find_module("__init__", [path])
 				try:
 					mod = imp.load_module("__init__", stream, "__init__.py", desc)
 				finally:
 					stream.close()
 
-
-				#mod = __import__(modname, globals(), locals(), [""])
+				self.plugins.append(path)
 				print mod.plugin_name
 
 
