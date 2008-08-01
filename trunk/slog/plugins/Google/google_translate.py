f34bfffabd3aa0315ef5ea23b7acd4e5347317e0
renatn <renatn@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2008-08-01 08:45:23 +0000

    Удалена куча хлама, и возможно сам класс тоже надо удалить

--- trunk/slog/plugins/Google/google_translate.py
+++ trunk/slog/plugins/Google/google_translate.py
@@ -1,64 +1,35 @@
 # -*- mode: python; coding: utf-8; -*-
 
-import urllib, urllib2
+import urllib
 
-LANG_ARABIC = "ar";
-LANG_CHINESE = "zh";
-LANG_ENGLISH = "en";
-LANG_FRENCH = "fr";
-LANG_GERMAN = "de";
-LANG_ITALIAN = "it";
-LANG_JAPANESE = "ja";
-LANG_KOREAN = "ko";
-LANG_PORTUGESE = "pt";
-LANG_RUSSIAN = "ru";
-LANG_SPANISH = "es";
-
-#TODO: Remove from here
-from slog.config import SlogConf
+LANG_ARABIC = "ar"
+LANG_CHINESE = "zh"
+LANG_ENGLISH = "en"
+LANG_FRENCH = "fr"
+LANG_GERMAN = "de"
+LANG_ITALIAN = "it"
+LANG_JAPANESE = "ja"
+LANG_KOREAN = "ko"
+LANG_PORTUGESE = "pt"
+LANG_RUSSIAN = "ru"
+LANG_SPANISH = "es"
 
 class GoogleEngine(object):
 
-	def __init__(self):
+	def __init__(self, proxy):
 
 		self.languages = {LANG_ARABIC:_("Arabic"), LANG_CHINESE:_("Chinese"), LANG_ENGLISH:_("English"),
 						LANG_FRENCH:_("French"), LANG_GERMAN:_("German"), LANG_ITALIAN:_("Italian"),
 						LANG_JAPANESE:_("Japanese"), LANG_KOREAN:_("Korean"), LANG_PORTUGESE:_("Portugese"),
 						LANG_RUSSIAN:_("Russian"), LANG_SPANISH:_("Spanish")}
 
-		# TODO: Remove
-		self.targets = ((LANG_ENGLISH, LANG_ARABIC), (LANG_ENGLISH, LANG_ITALIAN), (LANG_ENGLISH, LANG_CHINESE),
-					(LANG_ENGLISH, LANG_KOREAN), (LANG_ENGLISH, LANG_GERMAN), (LANG_ENGLISH, LANG_PORTUGESE),
-					(LANG_ENGLISH, LANG_RUSSIAN), (LANG_ENGLISH, LANG_FRENCH), (LANG_ENGLISH, LANG_JAPANESE),
-					(LANG_ENGLISH, LANG_SPANISH), (LANG_ARABIC, LANG_ENGLISH), (LANG_SPANISH, LANG_ENGLISH),
-					(LANG_ITALIAN, LANG_ENGLISH), (LANG_CHINESE, LANG_ENGLISH), (LANG_KOREAN, LANG_ENGLISH),
-					(LANG_GERMAN, LANG_ENGLISH), (LANG_GERMAN, LANG_FRENCH), (LANG_PORTUGESE, LANG_ENGLISH),
-					(LANG_RUSSIAN, LANG_ENGLISH), (LANG_FRENCH, LANG_ENGLISH), (LANG_FRENCH, LANG_GERMAN),
-					(LANG_JAPANESE, LANG_ENGLISH))
-
-	def get_targets(self):
-		""" Возвращает список строк, направлений перевода.		
-		"""
-		res = []
-		for target in self.targets:
-			target_str = self.languages[target[0]] + " - " + self.languages[target[1]]
-			res.append(target_str)
-		return res
-
-	def get_langs(self):
-		return self.languages
+		self.proxy = proxy
 
 	def translate(self, src, dst, text):
 		import socket
 		socket.setdefaulttimeout(10)
 
-		conf = SlogConf()
-		if conf.proxy != 0 and conf.proxy_host != "" and conf.proxy_port != 0:
-			proxy_url = "http://%s:%s" % (conf.proxy_host, conf.proxy_port)
-			proxy_support = urllib2.ProxyHandler({"http" : proxy_url})
-			opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
-		else:
-			opener = urllib2.build_opener(urllib2.HTTPHandler)
+		opener = self.proxy.get_http_opener()
 
 		pair = src + "|" + dst
 		params = urllib.urlencode({'langpair': pair, 'text': text.encode("utf8")})
