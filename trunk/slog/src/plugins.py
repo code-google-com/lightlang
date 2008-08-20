aa3b853a9d4cf83790fc06c64d02c1073eb934f0
renatn <renatn@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2008-08-20 08:53:36 +0000

    Fixed 24. Hot enable/disable plugins

--- trunk/slog/src/plugins.py
+++ trunk/slog/src/plugins.py
@@ -2,8 +2,10 @@
 
 import os
 import sys
-import imp
-import gtk
+import gtk, gobject
+
+from slog.TransPanel import TransView
+import slog.gui_helper as ghlp
 
 class PluginManager(object):
 	def __init__(self):
@@ -12,9 +14,9 @@ class PluginManager(object):
 		self.plugin_dirs = []
 		self.callbacks = []
 
-	def __fire_changed(self):
-		for callback in self.callbacks():
-			callback()
+	def __fire_changed(self, state, plugin_name, module):
+		for callback in self.callbacks:
+			callback(state, plugin_name, module)
 
 	def add_plugin_dir(self, directory):
 		self.plugin_dirs.append(directory)
@@ -51,23 +53,37 @@ class PluginManager(object):
 	def get_nth_plugin(self, number):
 		""" Возвращает подключенный плагин по номеру
 		"""
-		name = self.enabled_plugins.keys()[number]
+		list_names = self.enabled_plugins.keys()
+		if number < 0 or number >= len(list_names):
+			return None
+		name = list_names[number]
 		return self.enabled_plugins[name]
 
 	def get_enabled_plugin(self, name):
 		return self.enabled_plugins[name]
 
+	def get_plugin_num(self, name):
+		i = 0
+		for plugin_name in self.enabled_plugins.keys():
+			if plugin_name == name:
+				return i
+			i += 1
+		return -1
+
 	def enable_plugin(self, name):
 		""" Подключает плагин
 		"""
 		plugin = self.plugins[name]
-		self.enabled_plugins[name] = plugin.enable()
-		return self.enabled_plugins[name]
+		module = plugin.enable()
+		self.enabled_plugins[name] = module
+		self.__fire_changed(1, name, module)
 
 	def disable_plugin(self, name):
 		""" Отключает плагин
 		"""
+		module = self.enabled_plugins[name]
 		del self.enabled_plugins[name]
+		self.__fire_changed(0, name, module)
 
 	def configure_plugin(self, name, window):
 		plugin = self.enabled_plugins[name]
@@ -83,65 +99,72 @@ class PluginManager(object):
 class PluginView(object):
 
 	def __init__(self, gui, plugin_manager):
-		self.plugin_manager = plugin_manager
-		self.plugin_manager.connect(self.on_plugins_changed)
+		self.plugin_model = plugin_manager
+		self.plugin_model.connect(self.on_plugins_changed)
 		self.radio_group = None
 
-		self.sidebar = gui.get_widget("sideBar")
+		self.notebook = gui.get_widget("noteBook")
+		self.notebook.remove_page(0)
+		self.new_translate_page()
+		self.notebook.connect("button-press-event", self.on_notebook_pressed)
 
-		self.menu = gtk.Menu()
-		gui.get_widget("menuItemView").set_submenu(self.menu)
+		self.sidebar = gui.get_widget("sideBar")
+		self.plugins_menu = gui.get_widget("menuItemView")
+		self.plugins_menu.set_submenu(gtk.Menu())
 
 		self.accel_group = gtk.AccelGroup()
 		gui.get_widget("mainWindow").add_accel_group(self.accel_group)
+
+		self.menuitem_cut = gui.get_widget("menuitem_cut")
+		self.menuitem_cut.connect("activate", self.on_menuitem_cut_activate)
+
+		self.statusbar = gui.get_widget("statusBar")
+		self.context_id = self.statusbar.get_context_id("slog")
 	
 	def __create_plugin_menuitem(self, title, index):
 		menu_item = gtk.RadioMenuItem(self.radio_group, title)
 
-		if index < 10:
-			hotkey = ord(str(index))
-			menu_item.add_accelerator("activate", self.accel_group, hotkey, gtk.gdk.MOD1_MASK, gtk.ACCEL_VISIBLE)
-
-		menu_item.connect("activate", self.on_menuitem_view_activate, index-1)
-		
-		self.radio_group = menu_item
-
-		return menu_item
-	
-	def add_plugin(self, title, module):
-		index = len(self.menu.get_children()) + 1
-
-		panel = module.get_panel()
-		self.sidebar.append_page(panel)
+		hotkey = None
+		if index < 9:
+			a = "<Alt>%d" % (index + 1)
+			keyval, modmask = gtk.accelerator_parse(a)
+			menu_item.add_accelerator("activate", self.accel_group, keyval, modmask, gtk.ACCEL_VISIBLE)
 			
-		menu_item = self.__create_plugin_menuitem(title, index)
-		self.menu.append(menu_item)
-		menu_item.show()
+		menu_item.connect("toggled", self.on_menuitem_view_activate, index)
+		self.radio_group = menu_item
+		return menu_item
+
+	def load(self, plugins):
+		self.sidebar.remove_page(0)
+		for plugin in self.plugin_manager.get_available():
+
+			if plugin not in list_enabled:
+				continue
+
+			module = self.plugin_manager.enable_plugin(plugin)
+			module.connect("changed", self.on_status_changed)
+			
+			while gtk.events_pending():
+				gtk.main_iteration(False)
+
+	
+	def add_plugin(self, child, title):
+		self.sidebar.append_page(child)
+		index = self.sidebar.get_n_pages() - 1
+		m = self.__create_plugin_menuitem(title, index)
+		self.plugins_menu.get_submenu().append(m)
+		m.show()
 
 	def set_active(self, index):
-		self.menu.set_active(index)
-		m = self.menu.get_active()
-		m.set_active(True)
+		menu = self.plugins_menu.get_submenu()
+		menu_item = menu.get_children()[index]
+		menu_item.set_active(True)
 
 	def get_active(self):
 		return self.sidebar.get_current_page()
 
 	def get_model(self):
-		return self.plugin_manager
-
-	def on_menuitem_view_activate(self, widget, data):
-		""" Обработчик события активизации плагина, в
-			параметре <data> передается номер элемента.
-		"""
-		print "Index:", data
-		self.sidebar.set_current_page(data)
-		plugin = self.plugin_manager.get_nth_plugin(data)
-		plugin.grab_focus()
-
-	def clear_sidebar(self, sidebar):
-		count = sidebar.get_n_pages()
-		for i in range(count):
-			sidebar.remove_page(-1)
+		return self.plugin_model
 
 	def clear_menu(self, menu):
 		children = menu.get_children()
@@ -149,14 +172,87 @@ class PluginView(object):
 			menu.remove(c)
 			del c
 
-	def update(self):
-		self.clear_sidebar(self.sidebar)
-		self.clear_menu(self.menu)
+	def refresh_menu_plugins(self):
+		menu = self.plugins_menu.get_submenu()
+		self.clear_menu(menu)
 
-		for plugin_name in self.plugin_manager.get_enabled():
-			module = self.plugin_manager.get_enabled_plugin(plugin_name)
-			self.add_plugin(plugin_name, module)
+		index = 0
+		for plugin_name in self.plugin_model.get_enabled():
+			m = self.__create_plugin_menuitem(plugin_name, index)
+			self.plugins_menu.get_submenu().append(m)
+			m.show()
+			index += 1
 
-	def on_plugins_changed(self):
-		print "Changed"
+		self.set_active(0)
+
+	def new_translate_page(self, event=None):
+		""" Добавляет новую вкладку с окном перевода
+		"""
+		label = gtk.Label()
+		tv = TransView(label)
+		self.notebook.append_page(tv)
+		header = ghlp.create_tab_header(label, tv, self.on_close_tab_clicked)
+		self.notebook.set_tab_label(tv, header)
+		tv.show()
+		self.notebook.next_page()
+
+	def on_plugins_changed(self, event, name, module):
+		panel = module.get_panel()
+		if event == 0:
+			index = self.sidebar.page_num(panel)
+			self.sidebar.remove_page(index)
+			self.refresh_menu_plugins()
+			del module
+		else:
+			self.add_plugin(panel, name)
+			module.connect("translate_it", self.on_translate)
+			module.connect("changed", self.on_status_changed)
+
+	def on_menuitem_cut_activate(self, widget, data=None):
+		number = self.get_active()
+		module = self.plugin_model.get_nth_plugin(number)
+		if module != None:
+			module.clear()
+
+	def on_menuitem_view_activate(self, widget, data=None):
+		""" Обработчик события активизации плагина, в
+			параметре <data> передается номер элемента.
+		"""
+		module = self.plugin_model.get_nth_plugin(data)
+		if module != None:
+			self.sidebar.set_current_page(data)
+			module.grab_focus()
+
+	def on_notebook_pressed(self, widget, event, data=None):
+		""" Обработчик события двойного клика на панели вкладок
+		"""
+		if event.button == 1 and event.type == gtk.gdk._2BUTTON_PRESS:
+			self.new_translate_page()
+
+	def on_close_tab_clicked(self, widget, page):
+		# Always show one tab		
+		if self.notebook.get_n_pages() == 1:
+			page.clear()
+			return
+
+		idx = self.notebook.page_num(page)
+		self.notebook.remove_page(idx)
+		page.destroy()
+
+	#Thread safe update
+	def __set_translate(self, word, translate, newtab=False):
+		if newtab:
+			self.new_translate_page()
+
+		index = self.notebook.get_current_page()
+		tv = self.notebook.get_nth_page(index)
+		tv.set_translate(word, translate)
+
+	# Activated by Translate Engine
+	def on_translate(self, word, translate, newtab=False):
+		gobject.idle_add(self.__set_translate, word, translate, newtab)
+
+	def on_status_changed(self, msg):
+		self.statusbar.pop(self.context_id);
+		self.statusbar.push(self.context_id, msg)
 
