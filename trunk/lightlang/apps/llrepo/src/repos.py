ed278fcaa030abfe396bbc22859d4d398fc8a3a9
mdevaev <mdevaev@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2009-01-24 11:50:11 +0000

    Добавил нормальный файл конфигурации в llrepo

---
+++ trunk/lightlang/apps/llrepo/src/repos.py
@@ -0,0 +1,18 @@
+# Repositories list
+
+repos = [
+	{
+		"server" : "ftp.etc.edu.ru",
+		"port"   : 0,
+		"user"   : "",
+		"passwd" : "",
+		"root"   : "pub/soft/for_linux/lightlang"
+	}#,
+	#{
+	#	"server" : "ftp.server",
+	#	"port"   : 0
+	#	"user"   : "username"
+	#	"passwd" : "password"
+	#	"root"   : "path/to/root"
+	#}
+]
