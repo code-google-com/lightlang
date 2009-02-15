03045a0b6bbf9b650d4272512c6db53b31876b45
mdevaev <mdevaev@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2009-02-15 19:18:46 +0000

    Убрал меню сайтов-переводчиков из треевой иконки

--- trunk/lightlang/apps/xsl/src/pyqt4/TrayIcon.py
+++ trunk/lightlang/apps/xsl/src/pyqt4/TrayIcon.py
@@ -20,9 +20,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from PyQt4 import Qt
-try : # FIXME: Rrrrr... :-(
-	import TranslateSitesMenu
-except : pass
+#try : # FIXME: Rrrrr... :-(
+#	import TranslateSitesMenu
+#except : pass
 import Config
 import Const
 
@@ -87,12 +87,12 @@ class TrayIcon(Qt.QSystemTrayIcon) :
 		self.stop_spy_menu_action = self.tray_menu.addAction(Qt.QIcon(IconsDir+"stop_spy_16.png"),
 			self.tr("Stop Spy"), self.stopSpy)
 		self.stop_spy_menu_action.setEnabled(False)
-		try : # FIXME: Rrrrr... :-(
-			self.tray_menu.addSeparator()
-			self.translate_sites_menu = TranslateSitesMenu.TranslateSitesMenu(self.tr("Web translate"))
-			self.translate_sites_menu.setIcon(Qt.QIcon(IconsDir+"web_16.png"))
-			self.tray_menu.addMenu(self.translate_sites_menu)
-		except : pass
+		#try : # FIXME: Rrrrr... :-(
+		#	self.tray_menu.addSeparator()
+		#	self.translate_sites_menu = TranslateSitesMenu.TranslateSitesMenu(self.tr("Web translate"))
+		#	self.translate_sites_menu.setIcon(Qt.QIcon(IconsDir+"web_16.png"))
+		#	self.tray_menu.addMenu(self.translate_sites_menu)
+		#except : pass
 		self.tray_menu.addSeparator()
 		self.tray_menu.addAction(Qt.QIcon(IconsDir+"exit_16.png"), self.tr("Quit"), self.exit)
 		self.setContextMenu(self.tray_menu)
@@ -156,3 +156,4 @@ class TrayIcon(Qt.QSystemTrayIcon) :
 
 	def exitRequestSignal(self) :
 		self.emit(Qt.SIGNAL("exitRequest()"))
+
