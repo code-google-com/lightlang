30f291866ea4e0b90817252c25a1af55e33b09bc
mdevaev <mdevaev@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2009-10-08 18:09:14 +0000

    fix

--- trunk/lightlang/apps/xsl/src/pyqt4/EntitledMenu.py
+++ trunk/lightlang/apps/xsl/src/pyqt4/EntitledMenu.py
@@ -64,7 +64,7 @@ class EntitledMenu(Qt.QMenu) :
 		self.insertAction(before_action, fictive_action)
 
 		self.connect(fictive_button, Qt.SIGNAL("clicked()"), lambda : Qt.QDesktopServices.openUrl(
-			Qt.QUrl("http://lightlang.googlecode.com/svn/trunk/art/screenshots/lightlang/we.png")))
+			Qt.QUrl("http://lightlang.googlecode.com/svn/img/screenshots/lightlang/we.png")))
 
 		return fictive_action
 
