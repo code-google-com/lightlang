94d4c576ad4922d14d392a466b05009e48ba8f7f
mdevaev <mdevaev@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2010-01-22 13:58:44 +0000

    Рефакторинг и нормальное завершение работы потока

--- trunk/lightlang/apps/xsl/src/pyqt4/KeysGrabberThread.py
+++ trunk/lightlang/apps/xsl/src/pyqt4/KeysGrabberThread.py
@@ -21,9 +21,20 @@
 
 
 import Qt
+import Xlib.display
+import Xlib.XK
+import Xlib.X
 import Config
 import Const
-from KeysGrabberThreadBase import *
+
+
+#####
+Key_L = Xlib.XK.XK_L
+Key_F1 = Xlib.XK.XK_F1
+CtrlModifier = Xlib.X.ControlMask
+AltModifier = Xlib.X.Mod1Mask
+ShiftModifier = Xlib.X.ShiftMask
+WinModifier = Xlib.X.Mod4Mask
 
 
 #####
@@ -31,12 +42,70 @@ def tr(str) :
 	return Qt.QApplication.translate("@default", str)
 
 
-#####
-class KeysGrabberThread(KeysGrabberThreadBase) :
-	keys_grabber_thread_base_object = None
+##### Private #####
+class KeysGrabberThreadPrivate(Qt.QThread) :
+	def __init__(self, parent = None) :
+		Qt.QThread.__init__(self, parent)
+
+		#####
+
+		self.is_stopped_flag = True
+
+		self.handlers_list = []
+
+		self.display = Xlib.display.Display()
+		self.root = self.display.screen().root
+		self.root.change_attributes(event_mask = Xlib.X.PropertyChangeMask|Xlib.X.KeyPressMask)
+
+		#####
+
+		self.connect(Qt.QApplication.instance(), Qt.SIGNAL("aboutToQuit()"), self.stop)
+
+
+	### Public ###
+
+	def addHandler(self, key, modifier, handler) :
+		self.is_stopped_flag = True
+		if not self.wait(100) :
+			self.terminate()
+
+		key = self.display.keysym_to_keycode(key)
+		modifier = modifier & ~(Xlib.X.AnyModifier << 1)
+
+		self.handlers_list.append([key, modifier, handler])
+		self.root.grab_key(key, modifier, True, Xlib.X.GrabModeAsync, Xlib.X.GrabModeAsync)
+
+		self.is_stopped_flag = False
+		self.start()
+
+
+	### Private ###
+
+	def run(self) :
+		while not self.is_stopped_flag :
+			event = self.root.display.next_event()
+			if event.type != Xlib.X.KeyRelease :
+				continue
+
+			for handlers_list_item in self.handlers_list :
+				if event.detail == handlers_list_item[0] and (event.state & handlers_list_item[1]) == handlers_list_item[1] :
+					handlers_list_item[2]()
+
+	def stop(self) :
+		self.is_stopped_flag = True
+		if not self.wait(100) :
+			self.terminate()
+
+		for handlers_list_item in self.handlers_list :
+			self.root.ungrab_key(handlers_list_item[0], handlers_list_item[1])
+
+
+##### Public #####
+class KeysGrabberThread(KeysGrabberThreadPrivate) :
+	keys_grabber_thread_private_object = None
 
 	def __new__(self, parent = None) :
-		if self.keys_grabber_thread_base_object == None :
-			self.keys_grabber_thread_base_object = KeysGrabberThreadBase.__new__(self, parent)
-		return self.keys_grabber_thread_base_object
+		if self.keys_grabber_thread_private_object == None :
+			self.keys_grabber_thread_private_object = KeysGrabberThreadPrivate.__new__(self, parent)
+		return self.keys_grabber_thread_private_object
 
