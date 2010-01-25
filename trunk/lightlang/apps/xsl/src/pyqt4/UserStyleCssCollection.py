9612d5a38315ab0296d3d89c345fb19af1ecd260
mdevaev <mdevaev@7e159b8a-8345-0410-ba0a-4ff7fe149ffd>
2010-01-25 00:10:13 +0000

    fix

--- trunk/lightlang/apps/xsl/src/pyqt4/UserStyleCssCollection.py
+++ trunk/lightlang/apps/xsl/src/pyqt4/UserStyleCssCollection.py
@@ -129,7 +129,7 @@ def initUserStyleCssCollection() :
 	DictHeaderFontBoldFlagObject = False
 	DictHeaderFontItalicFlagObject = False
 	DictHeaderFontLargeFlagObject = False
-	DictHeaderFontColorObject = False
+	DictHeaderFontColorObject = Qt.QColor()
 
 	DictHeaderBackgroundColorObject = Qt.QColor()
 
